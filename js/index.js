cfda29acb973e6acb2487c56ab275e0cfb7e960d
SS-shasha <792574980@qq.com>
2018-10-11 15:35:23 +0800

    公司网站

---
+++ js/index.js
@@ -0,0 +1,17 @@
+﻿function highlightPage(){
+	if(!document.getElementsByTagName) return false;
+	if(!document.getElementById) return false;
+	var nav1=document.getElementById("nav1");
+	var links=nav1.getElementsByTagName("a");
+	var linkurl;
+	for(var i=0;i<links.length;i++){
+		linkurl=links[i].getAttribute("href");
+		if(window.location.href.indexOf(linkurl)!=-1){
+			links[i].className="here";
+		}
+	}
+}
+
+
+addLoadEvent(highlightPage());
+
